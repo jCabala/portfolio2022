f66f6c7a457b0bfd7c9d2feaf05d96a4fd5f5fb8
jCabala <kuba.cabala6@gmail.com>
2022-03-20 22:22:41 +0100

    jShop Project added

--- constants/constants.js
+++ constants/constants.js
@@ -2,40 +2,44 @@ export const projects = [
   {
     title: 'Workout Diary',
     description:
-      'This web application allows you to track your workouts. It is an online workout journal created with React and Bootstrap for frontend, and firebase for backend. It uses firebase authentication and firestore.',
+      'This web application allows you to track your workouts and more. It is an online workout journal created with React and Bootstrap for frontend, and firebase for backend. It uses firebase authentication and firestore.',
 
     sampleCrudentials: {
       email: 'test@gmail.com',
       password: 'test123',
     },
     image: '/images/projects/6.png',
-    tags: ['ReactJS', 'Bootstrap', 'Firebase'],
+    tags: ['ReactJS', 'Bootstrap', 'Firebase', 'Styled Components'],
     source: 'https://github.com/jCabala/workout-diary',
-    visit: 'https://hardcore-hamilton-0e9dfc.netlify.app/',
+    visit: 'https://workout-diary-jcabala.netlify.app',
     id: 6,
   },
-
+  {
+    title: 'jShop',
+    description:
+      'An online shop with working shopping cart. This website uses Material UI coupled with Styled Components for frontend and additionally Framer Motion for a custo modal element, which is used to display products. The products are fetched from fakestor api (https://fakestoreapi.com/)',
+    image: '/images/projects/7.png',
+    tags: [
+      'Typescript',
+      'React',
+      'Styled Components',
+      'MaterialUI',
+      'Framer Motion',
+    ],
+    source: 'https://github.com/jCabala/jShop',
+    visit: 'https://jshop-jcabala.netlify.app',
+    id: 2,
+  },
   {
     title: 'Portfolio & Blog',
     description:
       'My personal portfolio website which also has a blog connected. I write blog posts in markdown and use marked and grey-matter to dispaly them. I handled the conntact form using EmailJS which autamatically sends emails to me. This website was build using the Next.js framework. The blog may contain some sample posts with lorem ipsum while I am working on my first blogpost.',
     image: '/images/projects/5.png',
     tags: ['Next', 'EmailJS', 'Styled Components'],
-    source: 'https://github.com/jCabala/codementor-hubble',
+    source: 'https://github.com/jCabala/portfolio2022',
     visit: '/blog',
     isThisSite: true,
     id: 5,
-  },
-  {
-    title: 'Zespol MED',
-    description:
-      'This website was created for a music band from Nowy Sącz. I used strapi for content management and Material UI for some components. Im currently in the proccess of creating a blog for the same band.',
-    image: '/images/projects/4.png',
-    tags: ['React', 'Strapi', 'Material UI', 'EmailJS'],
-    source: 'https://google.com',
-    visit: 'https://zespol-med.netlify.app/',
-    id: 4,
-    commercial: true,
   },
   {
     title: 'IP Addresses Tracker',
@@ -44,18 +48,8 @@ export const projects = [
     image: '/images/projects/3.png',
     tags: ['React', 'Styled Components', 'Leaflet'],
     source: 'https://github.com/jCabala/ipTracker',
-    visit: 'https://modest-blackwell-4213d4.netlify.app/',
+    visit: 'https://iptracker-jcabala.netlify.app',
     id: 3,
-  },
-  {
-    title: 'Huddle',
-    description:
-      'A project from Frontend Mentor which required me to create a responsice landing page for a made-up company. It was a firs project in which I used styled components.',
-    image: '/images/projects/1.jpg',
-    tags: ['React', 'Styled Components'],
-    source: 'https://github.com/jCabala/codementor-hubble',
-    visit: 'https://kind-leavitt-16bd1e.netlify.app/',
-    id: 2,
   },
   {
     title: 'BBK Polska',
@@ -67,5 +61,27 @@ export const projects = [
     visit: 'https://bbkpl.com/',
     id: 1,
     commercial: true,
+  },
+  {
+    title: 'Zespol MED',
+    description:
+      'This website was created for a music band from Nowy Sącz. I used strapi for content management and Material UI for some components. Im currently in the proccess of creating a blog for the same band.',
+    image: '/images/projects/4.png',
+    tags: ['React', 'Strapi', 'Material UI', 'EmailJS'],
+    source: 'https://github.com/jCabala/zespolMed',
+    visit: 'https://zespol-med.netlify.app/',
+    id: 4,
+    commercial: true,
+  },
+
+  {
+    title: 'Huddle',
+    description:
+      'A project from Frontend Mentor which required me to create a responsice landing page for a made-up company. It was a firs project in which I used styled components.',
+    image: '/images/projects/1.jpg',
+    tags: ['React', 'Styled Components'],
+    source: 'https://github.com/jCabala/codementor-hubble',
+    visit: 'https://huddle-jcabala.netlify.app',
+    id: 2,
   },
 ];
